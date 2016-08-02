917da4a66d40ca6079def4e4e352960eb0287796
abadfish <kirsten.ofarrell@gmail.com>
2016-08-02 13:42:27 -0400

    refactored line_items, carts controllers

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :category
-  # accepts_nested_attributes_for :line_items
   has_many :line_items
 
   def self.available_items
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ActiveRecord::Base
   has_many :carts
   has_many :items, through: :carts
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
